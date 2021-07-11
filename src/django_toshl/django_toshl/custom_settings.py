f443c4b353d0dbd2f63ca05d250ff14ab17d501e
Javier Marcon <javiermarcon@gmail.com>
2021-07-11 16:30:50 -0300

    changing secret key

--- src/django_toshl/django_toshl/custom_settings.py
+++ src/django_toshl/django_toshl/custom_settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vkqf*z2tn7+4l+vzm%e+ho^(0+g@whsz0o!he9$z(zw5)6nf2s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
